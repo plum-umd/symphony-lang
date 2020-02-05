4463c5808a2b5bdc6c7b5c7e39c94870d76c917f
Bill Harris <wrharris@galois.com>
2020-02-04 18:21:07 -0800

    resets test comments

--- src/PSL/Interpreter.hs
+++ src/PSL/Interpreter.hs
@@ -341,12 +341,12 @@ testInterpreter = do
                 ]
         ]
       ]
-  -- testInterpreterExample "cmp"
-  -- testInterpreterExample "cmp-tutorial"
-  -- testInterpreterExample "euclid"
-  -- testInterpreterExample "msort"
-  -- testInterpreterExample "pfold"
-  testInterpreterExample "karmarkar"
+  testInterpreterExample "cmp"
+  testInterpreterExample "cmp-tutorial"
+  testInterpreterExample "euclid"
+  testInterpreterExample "msort"
+  testInterpreterExample "pfold"
+  -- testInterpreterExample "karmarkar"
   -- testInterpreterExample "atq"
   -- testInterpreterExample "cmp-split"
   -- testInterpreterExample "cmp-tutorial"
