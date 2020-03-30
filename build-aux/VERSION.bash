d6acc93fb37fb0b09905ce6c6457ae07e768e46c
Quinn Grier <quinn@stealthsoftwareinc.com>
2020-03-29 17:04:39 -0700

    Make VERSION.bash more robust

--- build-aux/VERSION.bash
+++ build-aux/VERSION.bash
@@ -5,21 +5,40 @@
 
 set -e; . src/bash/preludes/standard.bash
 
+r2='^(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)$'
+readonly r2
+
 if [[ -r VERSION ]]; then
-  cat VERSION
-  exit
+
+  v2=$(cat VERSION)
+  readonly v2
+
+  if [[ ! "$v2" =~ $r2 ]]; then
+    printf 'bad VERSION file content: %s\n' "$v2" >&2
+    exit 1
+  fi
+
+else
+
+  v1=$(git describe --match='v*' --long --always)
+  readonly v1
+
+  r1='^v(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)\.0-(0|[1-9][0-9]*)-g[0-9a-f]+$'
+  readonly r1
+
+  if [[ ! "$v1" =~ $r1 ]]; then
+    printf 'bad git describe output: %s\n' "$v1" >&2
+    exit 1
+  fi
+
+  v2=${BASH_REMATCH[1]}.${BASH_REMATCH[2]}.${BASH_REMATCH[3]}.${BASH_REMATCH[4]}
+  readonly v2
+
+  if [[ ! "$v2" =~ $r2 ]]; then
+    printf 'bad computed version number: %s\n' "$v2" >&2
+    exit 1
+  fi
+
 fi
 
-v=$(git describe --match='v*' --long --always)
-readonly v
-
-r='^v(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)\.0-(0|[1-9][0-9]*)-g[0-9a-f]+$'
-readonly r
-
-if [[ ! "$v" =~ $r ]]; then
-  printf 'bad git describe output: %s\n' "$v" >&2
-  printf 'did you mess up your v* tag?\n' >&2
-  exit 1
-fi
-
-printf '%s.%s.%s.%s\n' "${BASH_REMATCH[@]:1:4}"
+printf '%s\n' "$v2"
