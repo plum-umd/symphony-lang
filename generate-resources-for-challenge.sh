f477a30b1d6f4f2208f1dfed7f5d39d491c937ce
Quinn Grier <quinn@stealthsoftwareinc.com>
2020-04-25 14:10:40 -0700

    Print progress and wait for all runs to finish

--- generate-resources-for-challenge.sh
+++ generate-resources-for-challenge.sh
@@ -1,20 +1,46 @@
 #!/bin/bash
 
+set -e -u -o pipefail
+
+# Kill everything on exit, in particular on Ctrl-C.
+trap 'kill $(jobs -p) &>/dev/null || :' EXIT
+
+# Do a run, keeping track of the names and process IDs of all runs.
+pids=()
+names=()
+run() {
+  local psl=psl
+  # Prefer ./psl to psl if it exists.
+  if [[ -x ./psl ]]; then
+    psl=./psl
+  fi
+  $psl example -r $1 &
+  pid=$!
+  pids+=($pid)
+  names[$pid]=$1
+  printf 'started: %s\n' $1
+}
+
 for x in quick-sort-bgw quick-sort-spdz atq-bgw atq-spdz
 do
     for y in 8 12 16 20 24 28 32 36 40
     do
-	./psl example -r $x-$y &
+        run $x-$y
     done
 done
 for y in 8 16 32
 do
-    ./psl example -r msort-dedup-$y
+    run msort-dedup-$y
 done
 
-./psl example -r db-stats-small &
-./psl example -r db-stats-medium &
-./psl example -r db-stats-large &
-./psl example -r karmakar &
-./psl example -r gcd-bgv &
-./psl example -r gcd-gc &
+run db-stats-small
+run db-stats-medium
+run db-stats-large
+run karmakar
+run gcd-bgv
+run gcd-gc
+
+for pid in ${pids[@]}; do
+  printf 'waiting: %s\n' ${names[$pid]}
+  wait $pid
+done
