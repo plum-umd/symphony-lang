963e5b1913bbaf93ec3afee4470147232c6ab8f3
Quinn Grier <quinn@stealthsoftwareinc.com>
2020-04-27 00:06:22 -0700

    Work on the run RE GFI script

--- generate-resources-for-challenge.bash
+++ generate-resources-for-challenge.bash
@@ -6,8 +6,7 @@ trap 'kill $(jobs -p) &>/dev/null || :' EXIT
 full=0
 
 set x "$@"
-while :; do
-  shift
+while shift && (($#)); do
   case $1 in
     --full)
       full=1
@@ -23,13 +22,9 @@ done
 
 # Do a run and keep track of the process IDs of all runs.
 pids=()
+psl=${psl:-psl}
 run_psl() {
-  local psl=psl
-  # Prefer ./psl to psl if it exists.
-  if [[ -x ./psl ]]; then
-    psl=./psl
-  fi
-  printf 'PSL starting: %s\n' $1
+  printf 'psl starting: %s\n' $1
   >$1.log
   { printf 'Start time: ' && date; } >>$1.log
   (
@@ -38,9 +33,9 @@ run_psl() {
     { printf 'Stop time: ' && date; } >>$1.log
     printf 'Exit status: %s\n' $s >>$1.log
     if ((s == 0)); then
-      printf 'PSL done: %s\n' $1
+      printf 'psl done: %s\n' $1
     else
-      printf 'PSL done: %s (FAILED)\n' $1
+      printf 'psl done: %s (FAILED)\n' $1
     fi
   ) &
   pid=$!
@@ -51,7 +46,7 @@ for x in quick-sort-bgw quick-sort-spdz; do
   if ((full)); then
     quick_sort_ys='2 3 4 5 6 7 8 9 10'
   else
-    quick_sort_ys='2 3 4'
+    quick_sort_ys='2'
   fi
   for y in $quick_sort_ys; do
     run_psl $x-$y
@@ -62,7 +57,7 @@ for x in atq-bgw atq-spdz; do
   if ((full)); then
     atq_ys='8 12 16 20 24 28 32'
   else
-    atq_ys='8 12'
+    atq_ys=''
   fi
   for y in $atq_ys; do
     run_psl $x-$y
@@ -72,7 +67,7 @@ done
 if ((full)); then
   msort_ys='1 2 4 8 16'
 else
-  msort_ys='1 2'
+  msort_ys='1'
 fi
 for y in $msort_ys; do
   run_psl msort-dedup-$y
@@ -81,7 +76,7 @@ done
 if ((full)); then
   db_stats_ys='6 7 8 9 10 11 12'
 else
-  db_stats_ys='6 7 8 9'
+  db_stats_ys='6'
 fi
 for y in $db_stats_ys; do
   run_psl db-stats-$y
@@ -98,9 +93,17 @@ for pid in ${pids[@]}; do
 done
 
 run_re() {
-  printf 'traceEstimate: %s\n' $1
-  printf '\nResource estimation:\n\n' >>$1.log
-  python3 traceEstimate.py resources/$1.res "$@" >>$1.log
+  local e="$1"
+  shift
+  printf '\nResource estimation:\n\n' >>$e.log
+  s=0
+  python3 traceEstimate.py resources/$e.res "$@" >>$e.log || s=$?
+  printf 'Exit status: %s\n' $s >>$e.log
+  if ((s == 0)); then
+    printf 'traceEstimate: %s\n' $e
+  else
+    printf 'traceEstimate: %s (FAILED)\n' $e
+  fi
 }
 
 for y in $quick_sort_ys; do
