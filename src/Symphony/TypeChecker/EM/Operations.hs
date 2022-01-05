9b5e63325fd0190f14fd5ccf0fb9491c38751cd7
William Chung <wchung12326@gmail.com>
2022-01-05 14:54:11 -0500

    commit3

--- src/Symphony/TypeChecker/EM/Operations.hs
+++ src/Symphony/TypeChecker/EM/Operations.hs
@@ -11,13 +11,6 @@ runEM r s = unID ∘ unErrorT ∘ runRWST r s ∘ unEM
 evalEM ∷ ER → ES → EM a → EE ∨ a
 evalEM r s c = mapInr (\ (_ :* _ :* a) → a) $ runEM r s c
 
-evalEMIO ∷ ER → ES → 𝕊 → EM a → IO a
-runTMIO cxt name xM = case runTM cxt xM of
-  Inr x → return x
-  Inl e → do
-    pprint $ ppHorizontal [ ppErr ">", ppBD $ ppString name ]
-    printError e
-    abortIO
 
 evalEMErr ∷ (Monad m, MonadError Error m, STACK) ⇒ ER → ES → EM a → m a
 evalEMErr r s c = case evalEM r s c of
