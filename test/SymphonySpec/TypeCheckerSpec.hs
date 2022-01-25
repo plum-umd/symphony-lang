ce20c6428ba3c9ec740da926808c17fdeeccb783
William Chung <wchung12326@gmail.com>
2022-01-24 23:21:50 -0500

    Test

--- test/SymphonySpec/TypeCheckerSpec.hs
+++ test/SymphonySpec/TypeCheckerSpec.hs
@@ -44,10 +44,9 @@ spec = do
           b =  (UVM.AddTop (PowPSE (UVM.frhs [VarPE (UVM.var "A"), VarPE (UVM.var "C")]) )) 
           c = (UVM.AddTop (PowPSE (UVM.frhs [VarPE (UVM.var "A")]) )) 
           expr =  (IfE (UVM.𝐴 y (VarE (UVM.var "A"))) (UVM.𝐴 y (VarE (UVM.var "B"))) (UVM.𝐴 y (VarE (UVM.var "C"))) )
-          x  = (evalEM (ER {terSource = UVM.None, terMode = UVM.Top, terEnv = (UVM.assoc (UVM.frhs [ (UVM.var "A" , (SecT a (BaseT UnitT ))), (UVM.var "B" , (SecT b (BaseT UnitT ))) ])) }) () (synExpR expr)
-
-     in  case x of
-     UVM.Inr a -> a `shouldBe`  (SecT c  (BaseT UnitT))  
+          x  = (evalEM (ER {terSource = UVM.None, terMode = UVM.Top, terEnv = (UVM.assoc (UVM.frhs [ (UVM.var "A" , (SecT a (BaseT UnitT ))), (UVM.var "B" , (SecT b (BaseT UnitT ))) ])) }) () (synExpR expr) 
+      in case x of
+        UVM.Inr a -> a `shouldBe`  (SecT c  (BaseT UnitT))  
    {- it "() : nilexp" $  let x  = (evalEM (ER {terSource = UVM.None, terMode = UVM.Top, terEnv = (UVM.assoc (UVM.frhs [ (UVM.var "A" , (SecT UVM.Top (BaseT ℙT))) ])) }) () (synExpR (PrinSetE (PowPSE (UVM.single𝐿  (VarPE (UVM.var "A")) )) )))
      in  case x of
      UVM.Inr a -> a `shouldBe`  (SecT UVM.Top  (BaseT ℙsT))
