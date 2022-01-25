f21d6063e13327059a745f0848bd66f07ef86c0b
William Chung <wchung12326@gmail.com>
2022-01-24 23:29:18 -0500

    Test

--- src/Symphony/TypeChecker.hs
+++ src/Symphony/TypeChecker.hs
@@ -394,7 +394,7 @@ synIf e₁ e₂ e₃ =
     τ₂ ← c₂
     τ₃ ← c₃
     m ← askL terModeL
-    em  ← elabEMode
+    em  ← elabEMode m
     subcond ← (subtype (SecT em (BaseT 𝔹T)) τ₁)
     if subcond then do
       (top_wf τ₂ τ₃ m)
