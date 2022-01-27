0ff040aa02c67f5d20c9c7ad36387743edffe8b7
William Chung <wchung12326@gmail.com>
2022-01-27 01:40:32 -0500

    Test

--- src/Symphony/TypeChecker.hs
+++ src/Symphony/TypeChecker.hs
@@ -438,7 +438,7 @@ synCons eₕ eₜ =
       SecT em' (ListT _ τₜ)  →  do
         m ← askL terModeL
         m' ← elabEMode em' 
-        join_t ← (top_ty τ  τₜ )
+        join_t ← (top_wf τ  τₜ m)
         return SecT (inter_m m' m) join_t
     
   
