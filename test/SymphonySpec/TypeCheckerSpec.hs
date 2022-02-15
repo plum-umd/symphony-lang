44c72fb1514d8f9e5ca29396c5e1bae8d2763cb1
William Chung <wchung12326@gmail.com>
2022-02-15 02:18:14 -0500

    Test

--- test/SymphonySpec/TypeCheckerSpec.hs
+++ test/SymphonySpec/TypeCheckerSpec.hs
@@ -197,7 +197,7 @@ spec = do
           x  = (evalEM (ER {terSource = UVM.None, terMode = m, terEnv = (UVM.assoc (UVM.frhs [ (UVM.var "D" , (SecT UVM.Top (BaseT 𝔹T ))), (UVM.var "A" , (SecT a (BaseT UnitT ))), (UVM.var "B" , (SecT b (BaseT UnitT ))) ])) }) () (synExpR expr)) 
       in case x of
         UVM.Inr a -> a `shouldBe`  t
-       it "() : readbulbad" $ 
+    it "() : readbulbad" $ 
       let a =  (UVM.AddTop (PowPSE (UVM.frhs [VarPE (UVM.var "A"), VarPE (UVM.var "B")]) )) 
           b =  (UVM.AddTop (PowPSE (UVM.frhs [VarPE (UVM.var "A"), VarPE (UVM.var "C")]) )) 
           c = (UVM.AddTop (PowPSE (UVM.frhs [VarPE (UVM.var "A")]) )) 
