1b92a9cfbefa947acdd1b7756d55f073ef7e49ca
William <wchung12326@gmail.com>
2022-02-26 11:13:29 -0500

    Test

--- test/SymphonySpec/TypeCheckerSpec.hs
+++ test/SymphonySpec/TypeCheckerSpec.hs
@@ -301,7 +301,7 @@ spec = do
           expr = (AppE (nullExp lamexpr) (nullExp BulE))
           x  = (evalEM (ER {terSource = UVM.None, terMode = UVM.Top, terEnv = (UVM.assoc (UVM.frhs [ (UVM.var "D" , (SecT UVM.Top (BaseT 𝔹T ))), (UVM.var "A" , (SecT a (BaseT UnitT ))), (UVM.var "B" , (SecT b (BaseT UnitT ))) ])) }) () (synExpR expr))
         in case x of
-        UVM.Inr a -> a `shouldBe`  t
+        UVM.Inr a -> a `shouldBe`  t'
         UVM.Inl e -> expectationFailure $ Text.unpack $ UVM.frhs $ UVM.ppshow e
     it "() + () error" $
       let e = PrimE PlusO $ UVM.frhs $ [(nullExp BulE), (nullExp BulE)] in
