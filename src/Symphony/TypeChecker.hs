cef1a29cec60d3f160ff2bec4104f94065acc3dd
William <wchung12326@gmail.com>
2022-02-26 20:38:47 -0500

    Test

--- src/Symphony/TypeChecker.hs
+++ src/Symphony/TypeChecker.hs
@@ -564,18 +564,18 @@ synArraySize e =
   let c = synExp e 
   in 
     do
-    τ ← c
-    case τ of
-      (SecT loc (ArrT _ τ'))  → 
-        m  ← askL terModeL
-        l ← elabEMode loc₁
-        em ← elabMode m
-        --  dont need subcond  ←  (subtype τ (SecT m (RefT t')))
-        guardErr (m ≡ l₁) $
-          typeError "synArraySize: m /≡ l" $ frhs
-          [ ("m", pretty m)
-          , ("l", pretty l₁)
-          ]
+      τ ← c
+      case τ of
+        (SecT loc (ArrT _ τ'))  → 
+          m  ← askL terModeL
+          l ← elabEMode loc₁
+          em ← elabMode m
+          --  dont need subcond  ←  (subtype τ (SecT m (RefT t')))
+          guardErr (m ≡ l₁) $
+            typeError "synArraySize: m /≡ l" $ frhs
+            [ ("m", pretty m)
+            , ("l", pretty l₁)
+            ]
           return (SecT em (BaseT (ℕT 0)))
         
 -------------------
