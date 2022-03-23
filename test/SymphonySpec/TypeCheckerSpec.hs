74aa25839744b354cb522f5b8b682ff2d21e9fe9
William <wchung12326@gmail.com>
2022-03-23 13:29:50 -0400

    Test

--- test/SymphonySpec/TypeCheckerSpec.hs
+++ test/SymphonySpec/TypeCheckerSpec.hs
@@ -443,13 +443,13 @@ spec = do
           c = (UVM.AddTop (cpse))
           dpse = (PowPSE (UVM.frhs [ VarPE (UVM.var "C")]) )
           d = (UVM.AddTop (dpse))
-          t' =  (SecT c (BaseT UnitT))
-          t =  (SecT d (ShareT YaoNP d (BaseT UnitT)) )
+          t =  (SecT c (BaseT UnitT))
+          t' =  (SecT d (ShareT YaoNP d (BaseT UnitT)) )
           m = UVM.AddTop (UVM.pow𝐼 (UVM.iter (UVM.frhs [ (SinglePV "A"), (SinglePV "C")])))
           expr''' =  (BulE)
           expr'' = (ParE cpse (nullExp expr''')  )
-          expr = (ShareE YaoNP t'  aprin dpse (nullExp expr''))
-   
+          expr = (ShareE YaoNP t  aprin dpse (nullExp expr''))
+
           x  = (evalEM (ER {terSource = UVM.None, terMode = m, terEnv = (UVM.assoc (UVM.frhs [ (UVM.var "D" , (SecT UVM.Top (BaseT 𝔹T ))), (UVM.var "A" , (SecT a (BaseT UnitT ))), (UVM.var "B" , (SecT b (BaseT UnitT ))) ])) }) () (synExpR expr))
       in case x of
         UVM.Inr a -> a `shouldBe`  t
