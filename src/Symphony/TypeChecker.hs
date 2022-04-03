d4f38a56a40461db3b4932a5ba2c1aa9c4cbac1e
William <wchung12326@gmail.com>
2022-04-03 13:45:36 -0400

    Test

--- src/Symphony/TypeChecker.hs
+++ src/Symphony/TypeChecker.hs
@@ -634,8 +634,8 @@ synPar ρse₁ e₂ =
     if m' ≢ bot then
       localL terModeL m' c₂
     else
-      --todoError
-      return (SecT (AddTop (PowPSE empty𝐿)) (BaseT UnitT))
+      todoError
+      --return (SecT (AddTop (PowPSE empty𝐿)) (BaseT UnitT))
 
 checkPar ∷  PrinSetExp → Exp → Type → EM ()
 checkPar ρse₁ e₂ τ=
