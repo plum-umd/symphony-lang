25221e7943c06bc381a53eafaf885bf4b9e5267d
William Chung <wchung12326@gmail.com>
2022-04-12 12:35:07 -0400

    Test

--- src/Symphony/TypeChecker/Operations.hs
+++ src/Symphony/TypeChecker/Operations.hs
@@ -891,8 +891,7 @@ matchType τ ψ= case ψ of
               , ("l", pretty l)
               ] 
         (bindType τₗ ψₗ)
-     _ → typeError "matchType: ⊢ₘ _ ˡ→ _ ; type τ is not a sumtype" $ frhs
-              [ ("τ", pretty τ)] 
+     _ → typeError "matchType: ⊢ₘ _ ˡ→ _ ; type τ is not a sumtype" $ frhs [ ("τ", pretty τ)] 
   RP ψᵣ → case τ of
     (SecT loc (τₗ  :+: τᵣ)) → do
         m ← askL terModeL
