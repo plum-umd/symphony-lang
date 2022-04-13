180a2f3a5ccd4d127f39ea195ceafca113f6a403
William Chung <wchung12326@gmail.com>
2022-04-13 01:33:30 -0400

    Test]

--- src/Symphony/TypeChecker/Operations.hs
+++ src/Symphony/TypeChecker/Operations.hs
@@ -240,7 +240,7 @@ subtype tyS tyT = case tyS of
       SecT locT loctyT → do
         mcond ← (superemode locS locT)
         loccond ← (subtype_loc loctyS loctyT)
-        return ( loccond)
+        return (mcond ⩓ loccond)
       _ → return False
   _ → return False
 
