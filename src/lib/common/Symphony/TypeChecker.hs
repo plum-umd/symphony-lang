5eaa1b107ae554b25658df8a67f2aa64f70d8866
William Chung <wchung12326@gmail.com>
2022-05-17 12:37:13 -0400

    5/17

--- src/lib/common/Symphony/TypeChecker.hs
+++ src/lib/common/Symphony/TypeChecker.hs
@@ -1258,7 +1258,7 @@ chkExpR e τ =
     bigM ← askL terModeScopeL
     -- Check it is well formed
     _ ← (wf_type τ m bigM)
-    \case
+    case e of
       LE eₗ        → chkL eₗ τ
       RE eᵣ        → chkR eᵣ τ
       NilE        → chkNil τ
